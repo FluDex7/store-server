d931e5f7b4ea5dd7cf4b5569a396197c9b1a0e9f
FluDex7 <9e7erteagle@gmail.com>
2023-10-26 22:59:47 +0300

    --v3.1.3
    added:
    +__str__(self)
    +readme info

--- store/products/models.py
+++ store/products/models.py
@@ -10,6 +10,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)  # max_length - обязательный атрибут для типа CharField
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
