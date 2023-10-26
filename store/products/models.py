6ffae796455cc997634b53290ccd856604fa7742
FluDex7 <9e7erteagle@gmail.com>
2023-10-26 21:22:33 +0300

    --v3.1.2
    added:
    +model(table) Product
    +ForeignKey
    +Pillow-10.1.0 was installed for work with images

--- store/products/models.py
+++ store/products/models.py
@@ -4,7 +4,20 @@ from django.db import models
 # здесь будет создаватся таблица для БД
 # Модели = таблицы
 
-class ProductCategory(models):
+
+class ProductCategory(models.Model):
     # поле = тип
     name = models.CharField(max_length=128, unique=True)  # max_length - обязательный атрибут для типа CharField
     description = models.TextField(null=True, blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+    # Каскадное удаление - удаление всех связанных данных с данной категорией
+    # models.PROTECT - запретит удалять данные, пока у данной категории есть товары
+    # models.SET_DEFAULT - ставит значение по умолчанию, если категорию удалить
