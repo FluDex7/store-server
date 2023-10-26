148a58213c36c8193b15efdb4d74befe098a4426
FluDex7 <9e7erteagle@gmail.com>
2023-10-26 23:06:27 +0300

    --v3.1.3.2
    added:
    +register tables on admin-panel

--- store/products/admin.py
+++ store/products/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-# Регистрация, созданной в данном приложении, таблицы в админке
+# Регистрация, созданной в данном приложении, таблицы=модели для отображения её в Админ-панели
+from products.models import ProductCategory, Product
 
+admin.site.register(ProductCategory)
+admin.site.register(Product)
