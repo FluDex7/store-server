ccd7e4f53b11986010a899347b45c6c886e9e019
FluDex7 <9e7erteagle@gmail.com>
2023-10-26 23:16:11 +0300

    --v3.1.3.3
    added:
    +__str__ method
    +changed max_digit(to 8, now price can be 123456,78) and migrated
    +full data Product

--- store/products/models.py
+++ store/products/models.py
@@ -17,7 +17,7 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
@@ -25,3 +25,5 @@ class Product(models.Model):
         # models.PROTECT - запретит удалять данные, пока у данной категории есть товары
         # models.SET_DEFAULT - ставит значение по умолчанию, если категорию удалить
 
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
