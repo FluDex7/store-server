9bdda59061c98657c5ee3c262b48c75f5d7b1a2e
FluDex7 <9e7erteagle@gmail.com>
2023-10-27 04:03:22 +0300

    --v3.4
    +The backend of the site was fixed (there were problems with the database and fixtures)

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\Данил\PycharmProjects\store-server\venv\Scripts\python.exe
+#!C:\Users\Данил\PycharmProjects\pythonProject\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
