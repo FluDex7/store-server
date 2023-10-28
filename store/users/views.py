f5202750b67c20698d851349e15c2524c3b1e594
FluDex7 <9e7erteagle@gmail.com>
2023-10-28 10:51:23 +0300

    --v4.4
    added:
    +created forms.py
    +display form on login.html
    +data processing on views.py

--- store/users/views.py
+++ store/users/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse  # Принимает name=... и возвращает строку, по которому находиться адрес
+
+from users.models import User
+from users.forms import UserLoginForm
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':  # Если пользователь отправляет данные со страницы
+        form = UserLoginForm(data=request.POST)  # request.POST содержит данные с формы
+        if form.is_valid():  # Проверка формы на валидацию
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password) # Аутентификация пользователя
+                                                                           # (проверяет есть ли он в БД)
+            if user:  # Если пользователь существует, авторизоваться
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))  # Перенаправляет на главную страницу, если успешно
+    else:  # Если пользователь запрашивает саму страницу
+        form = UserLoginForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'users/login.html', context)
 
 
 def register(request):
