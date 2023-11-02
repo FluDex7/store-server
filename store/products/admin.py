bba409154e99750db25401ee6fb0181f3fd4e622
FluDex7 <9e7erteagle@gmail.com>
2023-11-02 14:07:25 +0300

    --v6.3
    +Admin-panel castomized

--- store/products/admin.py
+++ store/products/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 
 # Register your models here.
 # Регистрация, созданной в данном приложении, таблицы=модели для отображения её в Админ-панели
-from products.models import ProductCategory, Product
+from products.models import ProductCategory, Product, Basket
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')  # Отображение объектов
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')  # Отображение объекта
+    # readonly_fields = ('description',)
+    search_fields = ('name',)  # Поиск по имени
+    ordering = ('name',)  # Сортировка по алфавитному порядку -name для обратному отображению
+
+class BasketAdmin(admin.TabularInline):  # Для Foreign связи
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0  # default = 3, доп поля
--- store/users/admin.py
+++ store/users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from users.models import User
+from products.admin import BasketAdmin
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username','email', 'is_active')
+    search_fields = ('username',)
+    list_filter = ('is_active','is_staff')
+    inlines = (BasketAdmin,)
