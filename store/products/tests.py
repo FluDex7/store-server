8b80c385847c33dc48c8cf772d12d87931352544
FluDex7 <9e7erteagle@gmail.com>
2023-11-05 19:40:59 +0300

    --v9.1.2
    +ProductsListViewTestCase

--- store/products/tests.py
+++ store/products/tests.py
@@ -3,6 +3,8 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
+from products.models import Product
+
 
 class IndexViewTestCase(TestCase):
 
@@ -13,3 +15,17 @@ class IndexViewTestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'Store')
         self.assertTemplateUsed(response, 'products/index.html')
+
+
+class ProductsListViewTestCase(TestCase):
+    fixtures = ['Categories.json', 'Products.json']
+
+    def test_list(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        products = Product.objects.all()
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Каталог')
+        self.assertTemplateUsed(response, 'products/products.html')
+        self.assertEqual(response.context_data['object_list'], products)
