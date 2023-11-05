d071e911444176e92e9bc8720f38454f644e7321
FluDex7 <9e7erteagle@gmail.com>
2023-11-05 18:25:56 +0300

    --v9.1
    +TESTS:
    +IndexViewTestCase

--- store/products/tests.py
+++ store/products/tests.py
@@ -1,4 +1,15 @@
-# from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
-# Здесь создаются тесты для проекта (желательно создавать их в самом начале)
+from django.test import TestCase
+from django.urls import reverse
+
+
+class IndexViewTestCase(TestCase):
+
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store')
+        self.assertTemplateUsed(response, 'products/index.html')
