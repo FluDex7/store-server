a7032223d1e4c08bcc2ef92996d05c8c768cd325
FluDex7 <9e7erteagle@gmail.com>
2023-11-06 16:47:06 +0300

    --v9.1.4
    +ProductsListViewTestCase

--- store/products/tests.py
+++ store/products/tests.py
@@ -1,17 +1,15 @@
+import os  # для правильного запуска тестов
 from http import HTTPStatus
 
-#  Решение проблемы запуска тестов
-import os
-import django
-
-if 'env setting':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
-    django.setup()
-
+import django  # для правильного запуска тестов
 from django.test import TestCase
 from django.urls import reverse
 
-from products.models import Product
+from products.models import Product, ProductCategory
+
+if 'env setting':  # для правильного запуска тестов
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
+    django.setup()
 
 
 class IndexViewTestCase(TestCase):
@@ -29,12 +27,28 @@ class IndexViewTestCase(TestCase):
 class ProductsListViewTestCase(TestCase):
     fixtures = ['Categories.json', 'Products.json']
 
+    def setUp(self):
+        self.products = Product.objects.all()
+
     def test_list(self):
         path = reverse('products:index')
         response = self.client.get(path)
 
-        products = Product.objects.all()
+        self._common_test(response)
+        self.assertEqual(list(response.context_data['object_list']), list(self.products[:3]))
+
+    def test_list_category(self):
+        category = ProductCategory.objects.first()
+        path = reverse('products:category', kwargs={'category_id': category.id})
+        response = self.client.get(path)
+
+        self._common_test(response)
+        self.assertEqual(
+            list(response.context_data['object_list']),
+            list(self.products.filter(category_id=category.id))
+        )
+
+    def _common_test(self, response):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'Store - Каталог')
         self.assertEqual(response.context_data['view'].template_name, 'products/products.html')
-        self.assertEqual(response.context_data['object_list'], products)
