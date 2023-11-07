21d49140d3a5411efc5936040e1e409140f4393a
FluDex7 <9e7erteagle@gmail.com>
2023-11-07 17:49:18 +0300

    --v9.2.5
    Cache
    +third variable to cache page(cache object_list)

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from products.views import ProductsListView, basket_add, basket_remove
 
@@ -8,7 +7,7 @@ from products.views import ProductsListView, basket_add, basket_remove
 app_name = 'products'
 
 urlpatterns = [
-    path('', cache_page(30)(ProductsListView.as_view()), name='index'),
+    path('', ProductsListView.as_view(), name='index'),
     path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
     path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
