2a0aa2fd1388e8f2a3a45803a0a860fdfbfba3e5
FluDex7 <9e7erteagle@gmail.com>
2023-11-13 16:00:17 +0300

    --v10.5
    +get_status_display
    -order.html

--- store/orders/urls.py
+++ store/orders/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from orders.views import (CanceledTemplateView, OrderCreateView,
-                          SuccessTemplateView)
+                          SuccessTemplateView, OrderListView)
 
 app_name = 'orders'
 
 urlpatterns = [
     path('order_create/', OrderCreateView.as_view(), name='order_create'),
+    path('', OrderListView.as_view(), name='orders_list'),
     path('order_success/', SuccessTemplateView.as_view(), name='order_success'),
     path('order_cancelled/', CanceledTemplateView.as_view(), name='order_cancelled'),
 ]
